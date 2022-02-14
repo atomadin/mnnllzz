b763bfd8078bc4d6c663680dd1c8bd60ef5d9b5c
Andrea Tomadin <andrea.tomadin@unipi.it>
2022-02-14 14:30:47 +0100

    Update __main__.py

--- mnnllzz/__main__.py
+++ mnnllzz/__main__.py
@@ -7,8 +7,6 @@ import json
 
 from .mnnllzz import typeset_tests
 
-ver_string = "1.0.1"
-
 texfiles = glob.glob("*.tex")
 
 if (len(texfiles) < 1):
@@ -16,7 +14,7 @@ if (len(texfiles) < 1):
 elif (len(texfiles) > 1):
     sys.exit("More than one TeX file found in current directory.")
 else:
-    print("\n  mnnllzz %s\n" % ver_string)
+    print("\n  mnnllzz\n" % ver_string)
 
 files = open(texfiles[0],"r").read().split("MNNLLZZ")
 
